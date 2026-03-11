6586ad419d05bf7b27cabbef3a39fa489eac1a7a
Meraj Ansari <meraj6316@gmail.com>
2026-03-11 16:52:45 +0530

    feat: initialize JournalApp with HealthCheck and JournalEntryController

---
+++ journal/src/main/java/com/app/journal/controller/JournalEntryController.java
@@ -0,0 +1,8 @@
+package com.app.journal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JournalEntryController {
+
+}
---
+++ journal/src/main/java/com/app/journal/entity/JournalEntry.java
@@ -0,0 +1,7 @@
+package com.app.journal.entity;
+
+public class JournalEntry {
+    private long id;
+    private String title;
+    private String content;
+}
